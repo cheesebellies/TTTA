b85b1cf6acb9e21f425e0d04bf4f8988a212afc4
cheesebellies <84882131+cheesebellies@users.noreply.github.com>
2023-12-07 09:54:07 -0500

    Create main.asm

---
+++ main.asm
@@ -0,0 +1,19 @@
+;*******************************************************
+; PROGRAM NAME - Lab 0010 - Simple Algorithms
+;
+; PROGRAMMER - David Weir
+;
+; COMPILE - nasm -f elf64 -F dwarf -g Lab.asm
+;           ld -m elf_x86_64 -o lab Lab.o
+;
+; SYSTEM - Replit
+;
+; DATE - Started Jan. 12, 2023
+;
+; ISSUES - Don't use negative values when inputting numbers. No inputting numbers larger than 19 digits
+;
+; DESCRIPTION - Simple mathematic algorithms including 
+;               factoring, GCD, exponents, finding 
+;               digits, printing digits, and counting 
+;               digits.
+; *******************************************************
